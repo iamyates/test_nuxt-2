411f8f6f0ba28401c82f5c415d8f193bddb03666
iamyates <iamyates@sina.com>
2022-05-07 08:08:41 +0800

    demo for nuxt

---
+++ demo/components/Hi.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>hihihi2222222222333444 5555</div>
+</template>
---
+++ demo/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+     <p>88889991330--zzww33344555------jjjjjjjjjjj 6666 777 888  99</p>
+     <Hi/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
